a30d623210e066c2862e16fe00e1498a4fafdb39
xincas <ismrah.25@gmail.com>
2021-04-03 21:34:21 +0300

    interface for hashtable

--- CourseProject_SAOD/HashTable.h
+++ CourseProject_SAOD/HashTable.h
@@ -1,6 +1,21 @@
 #pragma once
+
+template <class K, class T>
 class HashTable
 {
-	
+private:
+	int HashTableSize_;
+	T* Data;
+
+
+	bool ReSize();
+
+public:
+	HashTable();
+	~HashTable();
+
+	bool AddData(T data);
+	bool RemoveData(T data);
+	T FindData(T data);
 };
 
